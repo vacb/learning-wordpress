6b21a472053cefaacae3425e91c4d675c0d78adc
Victoria Blackburn <victoria@sixtwo.tech>
2021-07-30 11:39:48 +0100

    Set up git repo

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Create function and point it to the css file. For default use get_stylesheet_uri() function
+// wp_enqueue_style( $handle, $src, $deps, $ver, $media );
+// wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+function university_files() {
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    
+}
+
+// Tell wordpress to run function at the right moment
+add_action('wp_enqueue_scripts', 'university_files');
