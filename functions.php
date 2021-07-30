30a4b1a806e7175a4f92a691213b2a829e269b9e
Victoria Blackburn <victoria@sixtwo.tech>
2021-07-30 15:28:54 +0100

    Add dynamic nav menu examples

--- functions.php
+++ functions.php
@@ -18,6 +18,10 @@ add_action('wp_enqueue_scripts', 'university_files');
 function university_features() {
     // Sets page title
     add_theme_support( 'title-tag' );
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
+    register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
+    // Then you can set up and edit menu in wp-admin
 }
 
 add_action('after_setup_theme', 'university_features');
