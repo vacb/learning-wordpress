70e4aa7e59b23389768a1881a4fb341ce3ea8a3a
Victoria Blackburn <victoria@sixtwo.tech>
2021-08-02 14:26:40 +0100

    Add program post type, add program archive page, add program custom field relationship for events

--- functions.php
+++ functions.php
@@ -49,6 +49,12 @@ function university_adjust_queries ($query) {
             )
             ));
     }
+    // Adjust programs query
+    if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
