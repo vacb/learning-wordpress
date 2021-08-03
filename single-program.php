a6bf04f5a64fd209891c77e4c020303bcaddfa93
Victoria Blackburn <victoria@sixtwo.tech>
2021-08-03 14:44:24 +0100

    Remove duplicate code by creating and implementing template part for event info

--- single-program.php
+++ single-program.php
@@ -8,9 +8,9 @@
 
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
-                <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
-                </p>
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
+            </p>
         </div>
         <div class="generic-content">
             <?php the_content(); ?>
@@ -88,36 +88,8 @@
                 echo '<h2 class="headline headline--medium">Upcoming ' .  get_the_title() . ' Events</h2>';
     
                 while($homepageEvents->have_posts()) {
-                    $homepageEvents->the_post(); ?>
-                        <div class="event-summary">
-                            <a class="event-summary__date t-center" href="#">
-                            <span class="event-summary__month">
-                                <?php 
-                                $eventDate = new DateTime(get_field('event_date'));
-                                echo $eventDate->format('M');
-                            ?>
-                            </span>
-                            <span class="event-summary__day">
-                            <?php 
-                                $eventDate = new DateTime(get_field('event_date'));
-                                echo $eventDate->format('d');
-                            ?>
-                            </span>
-                            </a>
-                            <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                            <p>
-                            <?php if (has_excerpt()) {
-                                    echo get_the_excerpt();
-                                    } else {
-                                    echo wp_trim_words(get_the_content(), 18);
-                                    }
-                                    ?>
-                                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                            </div>
-                        </div>
-    
-                    <?php 
+                    $homepageEvents->the_post();
+                    get_template_part('template-parts/content-event');
                 }
             }
 
