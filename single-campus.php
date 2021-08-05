fe8a0ec26f5f9909b1e93f9d01c322856404da16
Victoria Blackburn <victoria@sixtwo.tech>
2021-08-05 11:50:54 +0100

    Link google map tags to individual campus pages, update campus pages to show relevant programs

--- single-campus.php
+++ single-campus.php
@@ -9,45 +9,50 @@
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
-                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Campuses</a> <span class="metabox__main"><?php the_title(); ?></span>
             </p>
         </div>
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
 
+        <!-- Code for map - while loop removed as only need to show single campus -->
+        <div class="acf-map">
+            <?php $mapLocation = get_field('map_location'); ?>
+            <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+                <h3>
+                        <?php the_title(); ?>
+                </h3>
+                <?php echo $mapLocation['address']; ?>
+            </div>
+        </div>
+
         <?php
-        // Custom query to find academics related to this program of study
-            $relatedAcademics = new WP_Query(array(
+            $relatedPrograms = new WP_Query(array(
                 'posts_per_page' => -1,
-                'post_type' => 'academic',
+                'post_type' => 'program',
                 'orderby' => 'title',
                 'order' => 'ASC',
                 'meta_query' => array(
                 // One inner array for each filter
                     array(
-                        'key' => 'related_program',
+                        'key' => 'related_campus',
                         'compare' => 'LIKE',
                         // Concatenate in "" to search for "12" rather than 12, say - arrays are serialised to this avoids false positives
                         'value' => '"' . get_the_ID() . '"'
                     )
                 )
             ));
-            // Display the academics from custom query above
-            if ($relatedAcademics->have_posts()) {
-                echo '<hr class="section-break">';
-                echo '<h2 class="headline headline--medium">' . get_the_title() . ' Academics</h2>';
 
-                echo '<ul class="academic-cards">';
-                while($relatedAcademics->have_posts()) {
-                    $relatedAcademics->the_post(); ?>
-                    <li class="academic-card__list-item">
-                        <a class="academic-card" href="<?php the_permalink(); ?>">
-                            <img class="academic-card__image" src="<?php the_post_thumbnail_url('academicLandscape'); ?>">
-                            <span class="academic-card__name">
-                                <?php the_title(); ?>
-                            </span>
-                        </a>
+            if ($relatedPrograms->have_posts()) {
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">Programs Available At This Campus</h2>';
+
+                echo '<ul class="min-list link-list">';
+                while($relatedPrograms->have_posts()) {
+                    $relatedPrograms->the_post(); ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                     </li>
 
                     <?php 
@@ -84,7 +89,7 @@
                     )
                 )
             ));
-            // Display upcoming events from custom query above
+
             if ($homepageEvents->have_posts()) {
                 echo '<hr class="section-break">';
                 echo '<h2 class="headline headline--medium">Upcoming ' .  get_the_title() . ' Events</h2>';
@@ -93,25 +98,6 @@
                     $homepageEvents->the_post();
                     get_template_part('template-parts/content-event');
                 }
-            }
-
-            // Reset post data
-            wp_reset_postdata();
-
-            // Display related campuses
-            $relatedCampuses = get_field('related_campus');
-
-            if ($relatedCampuses) {
-                echo '<hr class="section-break">';
-                echo '<h2 class="headline headline--medium">' . get_the_title() . ' Is Available At These Campuses:</h2>';
-
-                echo '<ul class="min-list link-list">';
-                foreach($relatedCampuses as $campus) {
-                    ?><li>
-                        <a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a>
-                    </li><?php
-                };
-                echo '</ul>';
             }
 
             ?>
