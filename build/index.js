8b7bc4f7fa1e7696cb187a1783b31328db5ab018
Victoria Blackburn <victoria@sixtwo.tech>
2021-08-05 13:19:21 +0100

    Add keypress shortcuts for search overlay

--- build/index.js
+++ build/index.js
@@ -4057,21 +4057,43 @@ class Search {
     this.closeButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".search-overlay__close");
     this.searchOverlay = jquery__WEBPACK_IMPORTED_MODULE_0___default()(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   } // Events
 
 
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
-    this.closeButton.on("click", this.closeOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this)); // keyup fires once, keydown fires multiple times as long as you hold down the key
+    // Added loging in keyPressDispatcher below to ensure it only calls once either way
+
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).on("keydown", this.keyPressDispatcher.bind(this));
   } // Methods (function/action)
 
 
   openOverlay() {
-    this.searchOverlay.addClass("search-overlay--active");
+    this.searchOverlay.addClass("search-overlay--active"); // Add class that removes scroll bar as soon as overlay is opened by hiding overflows
+
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").addClass("body-no-scroll");
+    this.isOverlayOpen = true;
+    console.log("Open method ran");
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
+    console.log("Close method ran");
+  }
+
+  keyPressDispatcher(key) {
+    // console.log(key.keyCode);
+    if (key.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+
+    if (key.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
   }
 
 }
